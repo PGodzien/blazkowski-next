3591ccaf3522ebeab5e8d1d1bb298e21c3c9dd4b
Piotr Godzień <piotrgodzien@gmail.com>
2023-08-18 07:39:33 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import Nav from './components/nav'
 import Hero from './components/hero'
 import Services from './components/services'
 import Contact from './components/contact'
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: 'Barber Room Przemyśl - Damian Błażkowski',
+  description: 'Usługi barberskie w Przemyślu. Trymowanie brody, golenie brzytwą, strzyżenie, covering up gray, konnturowanie brody, barber full combo.',
+}
 export default function Home() {
   return (
     <>
